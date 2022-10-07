46af7b20525a6979acb0991d7926c8d4ecd6b777
TrungHieu-HCMUIT <19521515@gm.uit.edu.vn>
2022-10-07 16:36:03 +0700

    [FEAT] Add song

--- app.js
+++ app.js
@@ -125,12 +125,12 @@ const data = [
         avatar: './images/21.jpeg',
         mp3: './music/21.mp3'
     },
-    // { // Remove this song
-    //     song: 'Chầm Chậm Thích Em',
-    //     singer: 'Thành Nghiệp',
-    //     avatar: './images/22.jpeg',
-    //     mp3: './music/22.mp3'
-    // },
+    {
+        song: 'Chuyện Đôi Ta 「Lofi Ver」',
+        singer: 'Emcee L ft Muộii',
+        avatar: './images/22.jpeg',
+        mp3: './music/22.mp3'
+    },
     {
         song: 'Hẹn Một Mai',
         singer: 'Lưu Tuấn Phong',
